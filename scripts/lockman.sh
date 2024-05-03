ed1aa3ce544b129dfc36a368c1060fdea84f1756
Michael Behrens <mfbehrens99@gmail.com>
2024-05-03 02:07:10 +0200

    Add background image

--- scripts/lockman.sh
+++ scripts/lockman.sh
@@ -4,6 +4,7 @@ swayidle \
     timeout 10 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -c 550000
+loginctl lock-session
+swaylock -f -c 000000
 # Kills last background task so idle timer doesn't keep running
 kill %%
